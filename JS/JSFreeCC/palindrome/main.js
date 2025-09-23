ffd12da8ee2532a772023e9193573fcfff0e4d7e
Oscar Lopez <o.b.lopez@accenture.com>
2025-09-23 16:56:25 -0300

    palindrome

--- JS/JSFreeCC/palindrome/main.js
+++ JS/JSFreeCC/palindrome/main.js
@@ -8,6 +8,29 @@ When the #text-input element contains the text race car and the #check-btn eleme
 
 When the #text-input element contains the text not a palindrome and the #check-btn element is clicked, the #result element should contain the text not a palindrome is not a palindrome. */
 
-const textInput = document.getElementById('#text-input');
-console.log("salchipapa");
-console.log(textInput);
+const reverseString = (str) =>{
+  return str.split('').reverse().join('');
+}
+
+const inputFormatter = (str) => {
+    const regex = /[^\w]/g; //   /\s/g
+    let formattedInput = str.toLowerCase().trim().replace(regex, "");
+    return formattedInput;
+}
+
+const evaluateInput = () =>{
+    const textInput = document.querySelector("#text-input").value;
+    const textInputFormatted = reverseString(inputFormatter(textInput));
+    
+    if (inputFormatter(textInput) === textInputFormatted){
+        console.log(`${inputFormatter(textInput)}   ===  ${textInputFormatted}`);
+        console.log("Yes, they are bananas");
+        console.log("Why a banana");
+    } else{
+        console.log(`${inputFormatter(textInput)}   =|=  ${textInputFormatted}`);
+        console.log("No, they are not bananas");
+        console.log("Why NOT a banana");
+    }
+}
+
+
